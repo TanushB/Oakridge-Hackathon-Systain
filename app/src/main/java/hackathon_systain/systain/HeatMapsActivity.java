46a92698be6c583c7b50ec4a0f0741689a0418fd
Samarth007 <samarthgrover8@gmail.com>
2018-01-20 14:31:52 +0530

    code

---
+++ app/src/main/java/hackathon_systain/systain/HeatMapsActivity.java
@@ -0,0 +1,72 @@
+package hackathon_systain.systain;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.TileOverlay;
+import com.google.maps.android.heatmaps.Gradient;
+import com.google.maps.android.heatmaps.HeatmapTileProvider;
+
+public class HeatMapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private static final int ALT_HEATMAP_RADIUS = 25;
+    private static final int[] ALT_HEATMAP_GRADIENT_COLORS = {
+            Color.argb(0, 0, 255, 255),// transparent
+            Color.argb(255 / 3 * 2, 0, 255, 255),
+            Color.rgb(0, 191, 255),
+            Color.rgb(0, 0, 127),
+            Color.rgb(255, 0, 0)
+    };
+    public static final float[] ALT_HEATMAP_GRADIENT_START_POINTS = {
+            0.0f, 0.10f, 0.20f, 0.60f, 1.0f
+    };
+    public static final Gradient ALT_HEATMAP_GRADIENT = new Gradient(ALT_HEATMAP_GRADIENT_COLORS,
+            ALT_HEATMAP_GRADIENT_START_POINTS);
+    private HeatmapTileProvider mProvider;
+    private TileOverlay mOverlay;
+    private boolean mDefaultRadius = true;
+    private boolean mDefaultGradient = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_heat_maps);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+    public void changeRadius(View view) {
+        if (mDefaultRadius) {
+            mProvider.setRadius(ALT_HEATMAP_RADIUS);
+        } else {
+            mProvider.setRadius(HeatmapTileProvider.DEFAULT_RADIUS);
+        }
+        mOverlay.clearTileCache();
+        mDefaultRadius = !mDefaultRadius;
+    }
+    public void changeGradient(View view) {
+        if (mDefaultGradient) {
+            mProvider.setGradient(ALT_HEATMAP_GRADIENT);
+        } else {
+            mProvider.setGradient(HeatmapTileProvider.DEFAULT_GRADIENT);
+        }
+        mOverlay.clearTileCache();
+        mDefaultGradient = !mDefaultGradient;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // LatLng sydney = new LatLng(-34, 151);
+        // mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        // mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+}
